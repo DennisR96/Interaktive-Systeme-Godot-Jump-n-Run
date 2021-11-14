581b4bb4617644720013140d6722d4b39b7a2b7a
DennisR96 <81929072+DennisR96@users.noreply.github.com>
2021-11-14 13:55:26 +0100

    abc

--- Objekt.gd
+++
@@ -1,8 +0,0 @@
-extends RigidBody2D
-
-var picked = false
-
-func _input(event):
-	if Input.is_action_just_pressed("e")
-	
-
--- Player.gd
+++ Player.gd
@@ -2,30 +2,43 @@ class_name Player
 
 extends KinematicBody2D
 
-export var gravity = 8.1 
-export var speed = 50						# Geschwindigkei
+export var GRAVITY = 8.1 
+export var SPEED = 50						# Geschwindigkei
 export var jump_force = -240			
 
-var velocity = Vector2.ZERO
+var velocity = Vector2()
+
+var Projectile = preload("res://Projectile.tscn") 
+
+
+
+func shoot():
+	var p = Projectile.instance()
+	#owner.add_child(p)
+	p.transform = $Muzzle.transform
+
 
 func _physics_process(delta):
 	# Walking Animation
-	if Input.is_action_pressed("left"):
-		velocity.x = -speed
-		$AnimatedSprite.flip_h = false
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
 		$AnimatedSprite.play("walk")
-	elif Input.is_action_pressed("right"):
-		velocity.x = speed
-		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.flip_h = false
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
 		$AnimatedSprite.play ("walk")
+		$AnimatedSprite.flip_h = true
 	else:
 		velocity.x = 0
 		$AnimatedSprite.play("idle")
 		
 	# Jumping Animation
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = jump_force
 	
-	velocity.y += gravity
+	if Input.is_action_just_pressed("ui_focus_next"):
+		shoot()
+		
+	velocity.y += GRAVITY
 	velocity = move_and_slide(velocity, Vector2.UP)
-	#Hallo
+
