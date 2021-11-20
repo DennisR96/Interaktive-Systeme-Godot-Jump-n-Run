e7ed6de84a5cbd0b35b1a9913f1ecd70fae9dbf4
Kirsten <kirsten.grahl@outlook.de>
2021-11-20 16:45:06 +0100

    Ausprobieren

--- Game.gd
+++ Game.gd
@@ -3,8 +3,3 @@ extends Node
 func _on_Area2D_body_entered(body):
 	get_tree().reload_current_scene()
 	pass # Replace with function body.
-
-
-func _on_Button_pressed():
-	get_tree().change_scene("res://Node2D.tscn")
-	pass # Replace with function body.
