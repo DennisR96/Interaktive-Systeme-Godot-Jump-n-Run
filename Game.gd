12aeb10ecfd1c9c17555954b43fe5a7800a0d43c
Kirsten <kirsten.grahl@outlook.de>
2021-11-25 14:12:47 +0100

    Game schließen mit esc

--- Game.gd
+++ Game.gd
@@ -3,3 +3,8 @@ extends Node
 func _on_Area2D_body_entered(body):
 	if (body.get_name() == "Player"):
 		body.ouchFallzone()
+		
+func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()										# Quits Game
+		pass
