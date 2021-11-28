fc42e427a2301825a6b42aeb52d1ba9d5458ab28
DennisR96 <81929072+DennisR96@users.noreply.github.com>
2021-11-28 22:29:03 +0100

    Added Platforms

--- Game.gd
+++ Game.gd
@@ -1,5 +1,9 @@
 extends Node
 
+func _ready():
+	$Platform/MovingPlatform/AnimationPlayer2.play("LR")
+	$Platform/MovingPlatform2/AnimationPlayer2.play("LR")
+
 func _on_Area2D_body_entered(body):
 	if (body.get_name() == "Player"):
 		body.ouchFallzone()
