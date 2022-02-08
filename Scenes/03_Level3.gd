4ae5dbd86ad4059351f5ab2d0656cedba28cc96b
Kirsten <kirsten.grahl@outlook.de>
2022-02-08 17:10:36 +0100

    Update 03_Level3.gd

--- Scenes/03_Level3.gd
+++ Scenes/03_Level3.gd
@@ -9,4 +9,7 @@ func _physics_process(delta):
 		$HUD/Message.text = "Super! Du hast die Prinzessin gerettet! Drücke ESC um zum Menü zurückzukehren."
 		$HUD/Message.show()
 		$HUD/DarkerDisplay.show()
+		yield(get_tree().create_timer(5.0), "timeout")
+		$HUD/Message.hide()
+		$HUD/DarkerDisplay.hide()
 	
